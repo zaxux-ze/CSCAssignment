0f92d9a3672eeec4f224c9febb3bd62cf09c4119
Teng Zi En <zien01.18@ichat.sp.edu.sg>
2021-01-08 10:34:31 +0800

    Added in the c# part of consuming the web api

---
+++ Task1_CSCAssignment/ConsumeWebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ConsumeWebAPI.Global" Language="C#" %>
---
+++ Task1_CSCAssignment/ConsumeWebAPI/WeatherAPI.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ConsumeWebAPI
+{
+    public partial class WeatherAPI : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("https://api.data.gov.sg/v1/environment/2-hour-weather-forecast");
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+            webRequest.Accept = "application/json";
+            webRequest.Method = "GET";
+
+            HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
+            JsonDocument jsonObj = JsonDocument.Parse(webResponse.GetResponseStream());
+            JsonElement jsonElm = jsonObj.RootElement;
+            Response.Write(jsonElm.ToString());
+        }
+    }
+}
